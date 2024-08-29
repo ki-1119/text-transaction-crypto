b85b17ec43e88bc7e3a409c4060f3e1f1e58d29d
ki-1119 <coding.kei@gmail.com>
2024-08-29 22:41:36 +0200

    feat: complete to fetch graphql hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import cx from 'classnames';
 import { Inter } from 'next/font/google';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import BadgeLayout from './components/BadgeLayout';
 import Footer from './components/Footer';
 import HeaderNav from './components/HeaderNav';
@@ -11,40 +10,13 @@ import LastActivity from './components/LastActivity';
 import Preview from './components/Preview';
 import { TransactionContext } from './utils/contexts';
 import { GET_TRANSACTIONS } from './utils/queries';
+import useGraphQL from './utils/useGraphQL';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:8080/v1/graphql',
-  }),
-});
-
-const fetchData = async () => {
-  const response = await fetch('http://localhost:8080/v1/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-hasura-admin-secret': 'zxNnubCXe5ndyz8wco7okbeVlRCr91sYQaL4E6rNdKQ=',
-    },
-    body: JSON.stringify({
-      query: GET_TRANSACTIONS,
-    }),
-  });
-  if (!response.ok) {
-  }
-  const data = await response.json();
-  return data.data;
-};
-
 export default function Home() {
   const [transactions, setTransactions] = useState(0);
-  useEffect(() => {
-    fetchData().then((data) => {
-      setTransactions(data.logs);
-    });
-  }, []);
+  const [isLoading, data, error] = useGraphQL(GET_TRANSACTIONS);
   return (
     <TransactionContext.Provider value={transactions}>
       <div className={cx('bg-dark', inter.className)}>
